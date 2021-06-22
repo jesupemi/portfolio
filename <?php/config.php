df013f44744bb754da22faf8b24a94eadca555d8
jesupemi <76188975+jesupemi@users.noreply.github.com>
2021-06-21 18:57:24 -0700

    Create config.php

---
+++ <?php/config.php
@@ -0,0 +1,8 @@
+<?php 
+//db settings
+$db_host = "localhost";
+$db_name = "portfolio_";
+$db_password = "";
+$db_username = "root";
+$mysqli = new mysqli($db_host,$db_name,$db_password,$db_host);
+?>
